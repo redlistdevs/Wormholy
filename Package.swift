d1d4c23698a1264ebc5c5177f7be5dff5f41e7f0
Kody Holman <kody.holman@yourredlist.com>
2019-09-19 11:14:03 -0600

    Upgraded SwiftPackageManager

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Wormholy",
+    path: "Sources",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Wormholy",
-            targets: ["Wormholy"]),
+        .library(name: "Wormholy", targets: ["Wormholy"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
